321e483340daa70635843d5e073a4c6e94f27e9b
LouisAugustine <nzlouis.com@gmail.com>
2025-08-20 15:20:00 +1200

    Update Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer(props) {
     return (
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p className="col-md-4 mb-0 text-muted">&copy; {moment().year()} NZ Louis</p>
+                <p className="col-md-4 mb-0 text-muted">&copy; {moment().year()} NZLouis | Louis Lu. All rights reserved.</p>
                 <ul className="nav col-md-4 justify-content-end">
                 <li className="nav-item"><a href="https://nzlouis.com" target="_blank" rel="noreferrer" className="nav-link px-2 text-muted">About Me</a></li>
                 </ul>
